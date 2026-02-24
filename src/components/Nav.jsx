9eef7eed703546b1a38c5288c11ed257649d6981
Aisha Shaheed <ps.ashaheed@outlook.com>
2026-02-24 17:41:56 -0500

    uploaded Nav.jsx from components folder

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,1 +1,18 @@
 
+import { Link } from "react-router-dom";
+
+export default function Nav() {
+  return (
+    <div className="nav">
+        <Link to="/">
+        <div>CRYPTO PRICES</div>
+      </Link>
+      <Link to="/currencies">
+        <div>CURRENCIES</div>
+      </Link>
+      <Link to="/price">
+        <div>PRICES</div>
+      </Link>
+    </div>
+  );
+}
