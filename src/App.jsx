6f5cc7c73a381b2ebea610435a26c207eaa7bef9
Aisha Shaheed <ps.ashaheed@outlook.com>
2026-02-24 17:28:58 -0500

    Uploaded files from src folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,25 @@
+import "./Style.css";
 
+// Import Route and our components
+import { Route, Routes } from "react-router-dom";
+import Currencies from "./pages/Currencies.jsx";
+import Main from "./pages/Main.jsx";
+import Price from "./pages/Price.jsx";
+import Nav from "./components/Nav.jsx";
+
+
+
+
+export default function App() {
+  // We will use the Route component, path attribute, and element attribute to specify each route.
+  return (
+    <div className="App">
+        <Nav />
+      <Routes>
+        <Route path="/" element={<Main/>}/>
+        <Route path="/currencies" element={<Currencies/>}/>
+        <Route path="/price/:symbol" element={<Price/>}/>
+      </Routes>
+    </div>
+  );
+}
---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./style.css";
+import App from "./App";
+
+// Import BrowserRouter and rename it to Router
+import { BrowserRouter as Router } from "react-router-dom";
+
+// Wrap the App component with the Router component to enable the router features.
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </StrictMode>,
+);
